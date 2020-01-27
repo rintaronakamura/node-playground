07eff54b0e5e2f871beb07915515672157e391cb
residenti <n.queue.r@gmail.com>
2020-01-27 15:46:06 +0900

    X-Accel-Redirectを使ってS3上の画像を配信する.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
@@ -5,6 +7,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+console.log("process.env", process.env)
 var port = process.env.PORT || 8080;
 
 var router = require('./routes/v1/');
--- app/routes/v1/index.js
+++ app/routes/v1/index.js
@@ -1,18 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var AWS = require('aws-sdk');
 
 router.get('/image',function(req,res){
-  /*
-  response["Content-Type"] = "image/png"
-  response["Content-Disposition"] = "inline; filename={0}".format(
-    photo_name)
-  response['X-Accel-Redirect'] = "/media/{0}".format(photo.image)
-  */
+
+  AWS.config.update({
+    "accessKeyId": process.env.ACCESS_KEY_ID,
+    "secretAccessKey":process.env.SECRET_ACCESS_KEY,
+    "region": process.env.REGION
+  });
+
+  var params = {
+    Bucket: process.env.BUCKET,
+    Key: process.env.KEY,
+    Expires: Number(process.env.EXPIRES)
+  };
+
+  var s3 = new AWS.S3();
+  var url = s3.getSignedUrl('getObject', params);
+  console.log("The URL is", url);
+
+  res.header('Content-Type', 'image/jpg');
+  res.header('Content-Disposition', 'inline; filename=back_graund1.jpg');
+  res.header('X-Accel-Redirect', '/s3');
+  res.header('X-Reproxy-URL', url);
+  res.sendStatus(200);
+
+  /* nginx 上にある画像を返却する版.
   res.header('Content-Type', 'image/jpg');
   res.header('Content-Disposition', 'inline; filename=back_graund1.jpg');
   res.header('X-Accel-Redirect', '/media/back_graund1.jpg');
-
-  res.send(200);
+  res.sendStatus(200);
+  */
 });
 
 module.exports = router;
