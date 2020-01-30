6e361e26aa5770df3e5e003e220ef5075eb7ff9a
residenti <n.queue.r@gmail.com>
2020-01-30 18:01:31 +0900

    インメモリキャッシュの動作確認.

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 ## 検証項目一覧
 - Nginx の X-Accel-Redirect を使って、保護されたコンテンツ(S3上)を配信する方法
 - cluster モジュールの動作確認
+- インメモリキャッシュの動作確認
 
 ## セットアップ
 各コンテナの起動(`docker-compose.yml`があるディレクトリにて)。
@@ -30,6 +31,26 @@ API を呼び出すと交互にプロセスが反応する。
 [2] [PID 19] Request
 ```
 
+### インメモリキャッシュの動作確認
+cluster化された環境で動作させるとどうなるのかを検証。
+クライアントとして [3rd-Eden / memcached](https://github.com/3rd-Eden/memcached) を利用。
+結果、プロセス間でキャッシュしたデータを共有できた。
+```zsh
+node         | [1] [PID 16] Request
+node         | get data = undefined
+node         | set location value.
+node         | set success!
+
+node         | [1] [PID 16] Request
+node         | get data = back_graund1.jpg
+
+node         | [2] [PID 22] Request
+node         | get data = back_graund1.jpg
+
+node         | [1] [PID 16] Request
+node         | get data = back_graund1.jpg
+```
+
 ## 参考サイト
 ### Nginx の X-Accel-Redirect を使って、保護されたコンテンツ(S3上)を配信する方法
 - [DjangoでNginxのX-Accel-Redirectを使って、保護されたコンテンツを配信する方法](https://note.com/shimakaze_soft/n/nbeeeeeef6d8f)
@@ -41,3 +62,11 @@ API を呼び出すと交互にプロセスが反応する。
 
 ### cluster モジュールの動作確認
 - [Node.js の Cluster モジュールを使って Express サーバを並列化する - Corredor](http://neos21.hatenablog.com/entry/2019/04/18/080000)
+
+### インメモリキャッシュの動作確認
+- [使った node.js ライブラリ – メモリキャッシュ周り比較 « Ooharabucyou](https://www.bucyou.net/blog/1224)
+- [Webアプリケーションにおける正しいキャッシュ戦略 - Sansan Builders Box](https://buildersbox.corp-sansan.com/entry/2019/03/25/150000)
+- [Webアプリケーションにおけるキャッシュ戦略の比較 - kenju's blog](https://itiskj.hatenablog.com/entry/2017/08/18/000000)
+- [第1回　memcachedの基本：memcachedを知り尽くす｜gihyo.jp … 技術評論社](https://gihyo.jp/dev/feature/01/memcached/0001)
+- [memcached, Redis 何をどう使う - 補習ほぼ確](https://ave-h.hateblo.jp/entry/2018/08/09/234051)
+- [3rd-Eden / memcached](https://github.com/3rd-Eden/memcached)
--- app/routes/v1/index.js
+++ app/routes/v1/index.js
@@ -1,39 +1,60 @@
 const express = require('express');
-const router = express.Router();
 const AWS = require('aws-sdk');
 const cluster = require('cluster');
+const Memcached = require('memcached')
+
+const router = express.Router();
+const memcached = new Memcached('memcached:11211');
 
 router.get('/image',function(req,res){
   console.log(`[${cluster.worker.id}] [PID ${cluster.worker.process.pid}] Request`);
 
-  AWS.config.update({
-    "accessKeyId": process.env.ACCESS_KEY_ID,
-    "secretAccessKey":process.env.SECRET_ACCESS_KEY,
-    "region": process.env.REGION
+  memcached.get('location', function (err, data) {
+    if (err) { console.log("err", err); }
+
+    console.log("get data =", data);
+    let location = data;
+
+    if (!location) {
+      console.log("set location value.")
+      // DB から location を取得する処理.
+      location = 'back_graund1.jpg'
+      memcached.set('location', location, 30, function (err, data) {
+        if (err) { console.log("set err", err); }
+        console.log("set success!")
+      })
+    }
+
+    AWS.config.update({
+      "accessKeyId": process.env.ACCESS_KEY_ID,
+      "secretAccessKey":process.env.SECRET_ACCESS_KEY,
+      "region": process.env.REGION
+    });
+
+    const params = {
+      Bucket: process.env.BUCKET,
+      Key: location,
+      Expires: Number(process.env.EXPIRES)
+    };
+
+    const s3 = new AWS.S3();
+    const url = s3.getSignedUrl('getObject', params);
+    console.log("The URL is", url);
+
+    res.header('Content-Type', 'image/jpg');
+    res.header('Content-Disposition', 'inline; filename=back_graund1.jpg');
+    res.header('X-Accel-Redirect', '/s3');
+    res.header('X-Reproxy-URL', url);
+    res.sendStatus(200);
+
+    /* nginx 上にある画像を返却する版.
+    res.header('Content-Type', 'image/jpg');
+    res.header('Content-Disposition', 'inline; filename=back_graund1.jpg');
+    res.header('X-Accel-Redirect', '/media/back_graund1.jpg');
+    res.sendStatus(200);
+    */
   });
 
-  const params = {
-    Bucket: process.env.BUCKET,
-    Key: process.env.KEY,
-    Expires: Number(process.env.EXPIRES)
-  };
-
-  const s3 = new AWS.S3();
-  const url = s3.getSignedUrl('getObject', params);
-  console.log("The URL is", url);
-
-  res.header('Content-Type', 'image/jpg');
-  res.header('Content-Disposition', 'inline; filename=back_graund1.jpg');
-  res.header('X-Accel-Redirect', '/s3');
-  res.header('X-Reproxy-URL', url);
-  res.sendStatus(200);
-
-  /* nginx 上にある画像を返却する版.
-  res.header('Content-Type', 'image/jpg');
-  res.header('Content-Disposition', 'inline; filename=back_graund1.jpg');
-  res.header('X-Accel-Redirect', '/media/back_graund1.jpg');
-  res.sendStatus(200);
-  */
 });
 
 module.exports = router;
